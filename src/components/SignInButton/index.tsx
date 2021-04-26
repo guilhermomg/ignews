999f4eb1f5d272320da63066d541cb55c3bf169f
Guilhermo Gonzalez <guilhermomg@gmail.com>
2021-04-26 09:59:44 -0300

    NextAuth and FaunaDB implemented

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,21 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggedIn = false;
+    const [session] = useSession();
 
     return (
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => session ? signOut() : signIn('github')}
         >
-            <FaGithub color={isUserLoggedIn ? "#04d361" : "#eba417"} />
-            {isUserLoggedIn ? "Guilhermo González" : "Sign in with GitHub"}
-            {isUserLoggedIn && <FiX color="#737380" className={styles.closeIcon} />}
+            <FaGithub color={session ? "#04d361" : "#eba417"} />
+            {session ? session.user.name : "Sign in with GitHub"}
+            {session && <FiX color="#737380" className={styles.closeIcon} />}
         </button>
-
     );
 }
