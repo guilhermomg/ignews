f52e557b2302e41cb99c1e7c6f6e35d4576aa9a0
Guilhermo González <guilhermomg@gmail.com>
2021-04-28 22:29:50 -0300

    Beginning posts page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,40 @@
+
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts() {
+    return (
+        <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Como renomear vários arquivos de uma vez usando o terminal </strong>
+                        <p>Suponha que seu projeto tenha uma base de código com 150 arquivos JavaScript e você precisar migrar para TypeScript alterando as extensões dos arquivos. 🤔</p>
+                    </a>
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Como renomear vários arquivos de uma vez usando o terminal </strong>
+                        <p>Suponha que seu projeto tenha uma base de código com 150 arquivos JavaScript e você precisar migrar para TypeScript alterando as extensões dos arquivos. 🤔</p>
+                    </a>
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Como renomear vários arquivos de uma vez usando o terminal </strong>
+                        <p>Suponha que seu projeto tenha uma base de código com 150 arquivos JavaScript e você precisar migrar para TypeScript alterando as extensões dos arquivos. 🤔</p>
+                    </a>
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Como renomear vários arquivos de uma vez usando o terminal </strong>
+                        <p>Suponha que seu projeto tenha uma base de código com 150 arquivos JavaScript e você precisar migrar para TypeScript alterando as extensões dos arquivos. 🤔</p>
+                    </a>
+                </div>
+            </main>
+
+        </>
+
+    );
+}
