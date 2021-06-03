f300300fd47d6784d715c86ebf5adb6bbc22a22c
Guilhermo González <guilhermo.gonzalez@grupoboticario.com.br>
2021-06-03 17:05:40 -0300

    Pages tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import styles from './home.module.scss';
 interface HomeProps {
   product: {
     priceId: string;
-    amount: number;
+    amount: string;
   }
 }
 
@@ -25,10 +25,10 @@ export default function Home({ product }: HomeProps) {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for {product.amount} month</span>
+            <span>for {product.amount}/month</span>
           </p>
 
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
